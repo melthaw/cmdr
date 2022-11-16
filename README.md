972b6a25a60c359774bf32431e022d96b1609190
dz <melthaw@gmail.com>
2022-11-16 14:50:39 +0800

    update readme

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Get Started
 
-The `cmdr` is a cli tool to help user get cli tasks done quickly.
+Some of us know the regular commands and flags , and can get normal task done , but have to google how to do when the case gets complicated.
+The `cmdr` is a cli tool to help us get complicated cli tasks done quickly.
 
-* Know the regular commands and flags and can get normal task done but have to google how to do when the case gets complicated.
+* Supply classic command and flag combination to face one case.
+* And show the original command , just copy it and modify the args to match your case.
 
 
--- cmd/finder.go
+++ cmd/finder.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"fmt"
 	"github.com/spf13/cobra"
 	"os"
 	"os/exec"
@@ -10,6 +11,10 @@ var findCmd = &cobra.Command{
 	Use:   "find_big_file",
 	Short: "$> find / -type f -size +1G  -exec du -h {} \\;",
 	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("#############################################")
+		fmt.Println("# To find big file over 1G under the root path :")
+		fmt.Println("# find / -type f -size +1G  -exec du -h {} \\;")
+		fmt.Println("#############################################")
 		execCmd := exec.Command("find", " / -type f -size +1G  -exec du -h {} \\;")
 		execCmd.Stdout = os.Stdout
 		_ = execCmd.Run()
