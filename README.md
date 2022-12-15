142ca43aa04803f1919bd83cd4d0dd296ff8c080
dz <melthaw@gmail.com>
2022-12-15 09:34:03 +0800

    fix cmd exec

--- README.md
+++ README.md
@@ -6,4 +6,6 @@ The `cmdr` is a cli tool to help us get complicated cli tasks done quickly.
 * Supply classic command and flag combination to face one case.
 * And show the original command , just copy it and modify the args to match your case.
 
+## Sample
 
+> sudo go run main.go find_big_file
--- cmd/finder.go
+++ cmd/finder.go
@@ -3,7 +3,7 @@ package cmd
 import (
 	"fmt"
 	"github.com/spf13/cobra"
-	"os"
+	"log"
 	"os/exec"
 )
 
@@ -13,11 +13,28 @@ var findBigFileCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("#############################################")
 		fmt.Println("# To find big file over 1G under the root path :")
-		fmt.Println("# find / -type f -size +1G  -exec du -h {} \\;")
+		fmt.Println("# sudo find / -type f -size +1G")
+		fmt.Println("# or ")
+		fmt.Println("# sudo find / -type f -size +1G -exec du -h {} \\;")
 		fmt.Println("#############################################")
-		execCmd := exec.Command("find", " / -type f -size +1G  -exec du -h {} \\;")
-		execCmd.Stdout = os.Stdout
-		_ = execCmd.Run()
+		execCmd := exec.Command("find", "/", "-type", "f", "-size", "+1G")
+
+		stdout, err := execCmd.StdoutPipe()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		execCmd.Stderr = execCmd.Stdout
+		if err = execCmd.Start(); err != nil {
+			log.Fatalln(err)
+		}
+		for {
+			tmp := make([]byte, 1024)
+			_, err := stdout.Read(tmp)
+			if err != nil {
+				break
+			}
+			fmt.Print(string(tmp))
+		}
 	},
 }
 
